acfc7be5a99b4af1423e48dc23bb2dadd854d65b
Kevin <kevin.of.dahl@gmail.com>
2014-07-24 12:07:03 -0700

    log progress

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ f = open(sys.argv[1], 'r');
 
 network = {}
 
+print 'LOADING...'
+
 for edge in f:
 	nodes = edge.split()
 	for i in range(len(nodes)):
 		node = int(nodes[i])
 		if node not in network:
 			network[node] = {'friends':[]}
+			if len(network) % 250 == 0:
+				print '   ' + str(len(network))
 		network[node]['friends'].append(int(nodes[(i+1)%2]))
 	
 f.close()
@@ -23,6 +27,6 @@ for node in network:
 network = nodeList
 del nodeList
 
-print 'LOADED!'
+print 'LOADED'
 
-cascade.tryCascade(network, [1,2,3,4,5,6,7,8,9,10])
+cascade.tryCascade(network, range(100,500))
